d58a9caee957f712d5d593e6b60d0d0d75674023
ammar <ammar@wdodigital.com>
2024-07-10 13:53:52 +0400

    success

--- components/widget.tsx
+++ components/widget.tsx
@@ -26,95 +26,130 @@ export const Widget = ({ companySlug }: Props) => {
     undefined
   );
 
-  const {open:openSuccess,setOpen:setOpenSuccess} = useOpenSuccess()
+  const { open: openSuccess, setOpen: setOpenSuccess } = useOpenSuccess();
 
-  if(!company) return null
+  if (!company) return null;
 
   return (
     <div className="">
       <AnimatePresence>
         {open && (
           <Motion
-          style={{backgroundColor:company.widgetSettings.color}}
+            style={{ backgroundColor: company.widgetSettings.color }}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 20 }}
             className="     rounded-3xl   p-2  flex-1  z-[999]  fixed bottom-0   right-0   w-full   overflow-y-auto noScroll  "
           >
-           
-              {isLoading && (
-                <div className=" flex items-center justify-center   bg-white flex-col    h-[98.9vh]">
-                  <Loader size={20} className="animate-spin block" />
-                  <p className="mt-4 text-xl text-gray-400">Loading Data...</p>
-                </div>
-              )}
-              {isError && (
-                <div className="bg-white h-[98.6vh] flex items-center justify-center">
-                  <p className="mt-4 text-xl font-semibold text-rose-500">
-                    Oops...Something went wrong!
-                  </p>
-                </div>
-              )}
-              {company && (
-                <div className="bg-white rounded-3xl sm:rounded-2xl h-[98.6vh]   overflow-y-hidden relative ">
-                  <div className="flex items-center justify-between px-3   border-b h-12">
-                  { selectedForm ?  <button
+            {isLoading && (
+              <div className=" flex items-center justify-center   bg-white flex-col    h-[98.9vh]">
+                <Loader size={20} className="animate-spin block" />
+                <p className="mt-4 text-xl text-gray-400">Loading Data...</p>
+              </div>
+            )}
+            {isError && (
+              <div className="bg-white h-[98.6vh] flex items-center justify-center">
+                <p className="mt-4 text-xl font-semibold text-rose-500">
+                  Oops...Something went wrong!
+                </p>
+              </div>
+            )}
+            {company && (
+              <div className="bg-white rounded-3xl sm:rounded-2xl h-[98.6vh]   overflow-y-hidden relative ">
+                <div className="flex items-center justify-between px-3   border-b h-12">
+                  {selectedForm ? (
+                    <button
                       type="button"
                       title="back"
                       onClick={() => setSelectedForm(undefined)}
                       className="flex  items-center justify-center   gap-2   "
                     >
-                     <span className="w-8 h-8 rounded-full bg-white items-center justify-center  flex hover:bg-muted transition"><ArrowLeft /> </span> <span>Go Back</span>
-                    </button> : <span/>}
-                    {!selectedForm && company.forms.length > 1 &&  <p>Choose Form</p>}
-                    <button
-                      type="button"
-                      title="close"
-                      onClick={() => {setOpen(false);setSelectedForm(undefined); window.parent.postMessage(  'close-widget' , '*')}}
-                      className="flex  items-center justify-center w-8 h-8   rounded-full bg-white hover:bg-muted transition"
-                    >
-                      <XIcon />
+                      <span className="w-8 h-8 rounded-full bg-white items-center justify-center  flex hover:bg-muted transition">
+                        <ArrowLeft />{" "}
+                      </span>{" "}
+                      <span>Go Back</span>
                     </button>
-                  </div>
-
-                {openSuccess ? <Motion
-                   initial={{ opacity: 0, y: 20 }}
-                   animate={{ opacity: 1, y: 0 }}
-                   exit={{ opacity: 0, y: 20 }}
-                className="h-[calc(98vh-50px)] flex items-center flex-col justify-center">
-                  {company.widgetSettings.thankyouText}
+                  ) : (
+                    <span />
+                  )}
+                  {!selectedForm && company.forms.length > 1 && (
+                    <p>Choose Form</p>
+                  )}
                   <button
-                  onClick={()=>{setOpenSuccess(false);setSelectedForm(undefined)}}
-                  className= "p-1 border-transparent  border-2 rounded-md hover:opacity-90 transition  justify-center  flex items-center ml-auto fixed bottom-0 right-0 w-[150px] h-[50px] text-sm text-white     font-semibold "
-                    style={{backgroundColor:company.widgetSettings.color}}
-                  >Close</button>
-                </Motion> :  <Motion
-                     initial={{ opacity: 0, y: 20 }}
-                     animate={{ opacity: 1, y: 0 }}
-                     exit={{ opacity: 0, y: 20 }}
-                ><WidgetContent
-                    selectedForm={selectedForm}
-                    setSelectedForm={setSelectedForm}
-                  /></Motion>}
+                    type="button"
+                    title="close"
+                    onClick={() => {
+                      setOpen(false);
+                      setSelectedForm(undefined);
+                      window.parent.postMessage("close-widget", "*");
+                    }}
+                    className="flex  items-center justify-center w-8 h-8   rounded-full bg-white hover:bg-muted transition"
+                  >
+                    <XIcon />
+                  </button>
                 </div>
-              )}
-        
+                <AnimatePresence mode="wait">
+                  {openSuccess ? (
+                    <Motion
+                    key={'thank you'}
+                      initial={{ opacity: 0, y: -20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -20 }}
+                      className="h-[calc(98vh-50px)] flex items-center flex-col justify-center p-4"
+                    >
+                      {company.widgetSettings.thankyouText}
+                      <button
+                        onClick={() => {
+                          setOpenSuccess(false);
+                          setSelectedForm(undefined);
+                        }}
+                        className="p-1 border-transparent  border-2 rounded-md hover:opacity-90 transition  justify-center  flex items-center  mt-auto  w-full h-[50px] text-sm text-white     font-semibold "
+                        style={{
+                          backgroundColor: company.widgetSettings.color,
+                        }}
+                      >
+                        Close
+                      </button>
+                    </Motion>
+                  ) : (
+                    <Motion
+                    key={'content'}
+                      initial={{ opacity: 0, y: -20 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -20 }}
+                    >
+                      <WidgetContent
+                        selectedForm={selectedForm}
+                        setSelectedForm={setSelectedForm}
+                      />
+                    </Motion>
+                  )}
+                </AnimatePresence>
+              </div>
+            )}
           </Motion>
         )}
       </AnimatePresence>
       <button
-      style={{backgroundColor:company.widgetSettings.color}}
-        onClick={() => {setOpen((prev) => !prev); window.parent.postMessage(open ? 'close-widget' : 'open-widget', '*')}}
+        style={{ backgroundColor: company.widgetSettings.color }}
+        onClick={() => {
+          setOpen((prev) => !prev);
+          window.parent.postMessage(open ? "close-widget" : "open-widget", "*");
+        }}
         type="button"
         className={cn(
           "p-1 border-transparent  border-2 rounded-md hover:opacity-90 transition  justify-center  flex items-center ml-auto fixed bottom-0 right-0 w-[150px] h-[50px] text-sm text-white     font-semibold ",
           open && "opacity-0 pointer-events-none "
         )}
       >
-       {company.widgetSettings.widgetButtonText}
+        {company.widgetSettings.widgetButtonText}
         <ChevronDown
-        size={13}
-          className={open ? "rotate-180 transition ml-3 shrink-0" : "transition ml-3 shrink-0"}
+          size={13}
+          className={
+            open
+              ? "rotate-180 transition ml-3 shrink-0"
+              : "transition ml-3 shrink-0"
+          }
         />
       </button>
     </div>
